10b094a0beab973b63fe1d278fb9bf8402e76bd5
NamahageTahara <0802creo@gmail.com>
2018-12-12 16:00:53 +0900

    Add files via upload

--- frontend/src/actions/UserAction.js
+++ frontend/src/actions/UserAction.js
@@ -2,13 +2,14 @@ import { ActionTypes } from 'const';
 
 class UserAction {
 
-  static setUser({ name, win_count, lost_count, game }) {
+  static setUser({ name, tip, refund_tip, game,refunding }) {
     return {
       type: ActionTypes.SET_USER,
       name,      // User name
-      win_count, // Users win count
-      lost_count,// Users lost count
-      game,      // Users current Gamestate
+      tip, // Users win count
+      refund_tip,// Users lost count
+      game, 
+      refunding     // Users current Gamestate
     }
   }
 
